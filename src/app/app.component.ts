86d138463a36b03c9f5d7a18664096cce5046f02
David Oetterli <david.oetterli@hftm.ch>
2024-11-01 00:34:27 +0100

    style: add flex styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { EMPTY, Observable } from 'rxjs';
+import { Blogs } from './model/blogs';
+import { BlogService } from './services/blog.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BlogComponent, AsyncPipe, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blog-app';
+
+  blogs$: Observable<Blogs> = EMPTY;
+
+  constructor(private blogService: BlogService) {}
+
+  ngOnInit(): void {
+    this.blogs$ = this.blogService.loadBlogs();
+  }
 }
