f1db4506a78b6a08a04c957831af99f23a95012e
David Oetterli <david.oetterli@hftm.ch>
2024-12-17 09:06:01 +0100

    fix: fixed failed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'blog-app' title`, () => {
+  it(`should have the 'BlogApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blog-app');
+    expect(app.title).toEqual('BlogApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, blog-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('BlogApp'); // Testet, ob der Titel korrekt im h1 angezeigt wird
   });
 });
