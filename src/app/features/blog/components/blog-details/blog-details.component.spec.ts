4a2956bd63ef6c06ed02324f6154690806731502
David Oetterli <david.oetterli@hftm.ch>
2024-12-17 09:20:52 +0100

    fix: fixed failed tests

--- src/app/features/blog/components/blog-details/blog-details.component.spec.ts
+++ src/app/features/blog/components/blog-details/blog-details.component.spec.ts
@@ -1,19 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BlogDetailsComponent } from './blog-details.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BlogService } from '../../services/blog.service';
 
 describe('BlogDetailsComponent', () => {
   let component: BlogDetailsComponent;
   let fixture: ComponentFixture<BlogDetailsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BlogDetailsComponent],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [BlogDetailsComponent, HttpClientTestingModule],
+      providers: [
+        BlogService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                blog: { title: 'Test Blog', content: 'This is a test blog.' },
+              },
+            },
+            paramMap: of({ id: '1' }),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BlogDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
