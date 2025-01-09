fe49df44904b9d3c8de11d092abefbee21c574b5
David Oetterli <david.oetterli@hftm.ch>
2025-01-09 19:00:46 +0100

    feat: delete header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet, HeaderComponent],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, SidebarComponent],
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
